9bb61d4532dbadb5645d2f87a5bc1911131962d4
Ryan Rowland <w.rowland.ryan@gmail.com>
2019-05-15 22:22:11 -0500

    Day11 - added fullscreen button and native html5 fullscreen feature

--- 11 - Custom Video Player/scripts.js
+++ 11 - Custom Video Player/scripts.js
@@ -7,9 +7,12 @@ const progressBar = player.querySelector('.progress__filled');
 const toggle = player.querySelector('.toggle');
 const skipButtons = player.querySelectorAll('[data-skip]');
 const ranges = player.querySelectorAll('.player__slider');
+const fullscreen = player.querySelector('.fullscreen');
 
 /* Build Functions */
 function togglePlay() {
+  // if video is fullscreen, prevent toggle from interfering with native scripts
+  if (document.fullscreen) return;
   video.paused ? video.play() : video.pause();
 }
 
@@ -35,6 +38,23 @@ function scrub(e) {
   video.currentTime = scrubTime;
 }
 
+// use Fullscreen Api to go fullscreen
+// problem: uses native HTML5 video player in fullscreen
+function fullscreenHandler() {
+  if (video.requestFullscreen) {
+    video.requestFullscreen();
+  } 
+  else if (video.mozRequestFullScreen) {
+    video.mozRequestFullScreen();
+  } 
+  else if (video.webkitRequestFullscreen) {
+    video.webkitRequestFullscreen();
+  } 
+  else if (video.msRequestFullscreen) { 
+    video.msRequestFullscreen();
+  }
+}
+
 /* Add Event Listeners */
 video.addEventListener('click', togglePlay);
 video.addEventListener('play', updateButton);
@@ -54,3 +74,5 @@ progress.addEventListener('click', scrub);
 progress.addEventListener('mousemove', (e) => mousedown && scrub(e));
 progress.addEventListener('mousedown', () => mousedown = true);
 progress.addEventListener('mouseup', () => mousedown = false);
+
+fullscreen.addEventListener('click', fullscreenHandler);
