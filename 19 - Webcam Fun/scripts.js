06a0ceb96dfa6ffa9c8990fcafb50bd189cd574a
Ryan Rowland <w.rowland.ryan@gmail.com>
2019-05-20 02:14:49 -0500

    Day19 - added basic webcam capturing and canvas picture saving

--- 19 - Webcam Fun/scripts.js
+++ 19 - Webcam Fun/scripts.js
@@ -1,5 +1,53 @@
+// selectors
 const video = document.querySelector('.player');
 const canvas = document.querySelector('.photo');
 const ctx = canvas.getContext('2d');
 const strip = document.querySelector('.strip');
 const snap = document.querySelector('.snap');
+
+async function getVideo() {
+  try {
+    const localMediaStream = await navigator.mediaDevices.getUserMedia({ video: true, audio: false });
+    console.log(localMediaStream);
+    // video.src = window.URL.createObjectURL(localMediaStream);
+    // ^^ shown in video, is deprecated now
+    video.srcObject = localMediaStream;
+    video.play();
+  }
+  catch (err) {
+    alert('Kinda useless without media access, sorry.');
+    console.error(err);
+  }
+}
+
+function paintToCanvas() {
+  const width = video.videoWidth;
+  const height = video.videoHeight;
+
+  canvas.width = width;
+  canvas.height = height;
+
+  return setInterval(() => {
+    ctx.drawImage(video, 0, 0, width, height);
+    const pixels = ctx.getImageData(0, 0, width, height);
+  }, 16);
+}
+
+function takePhoto() {
+  // play sound
+  snap.currentTime = 0;
+  snap.play();
+
+  // take data out of canvas
+  const data = canvas.toDataURL("image/jpeg");
+  const link = document.createElement('a');
+  link.href = data;
+  link.setAttribute('download', 'handsome');
+  link.textContent = 'Download Image';
+  link.innerHTML = `<img src="${data}" alt="Handsome photo" />`;
+  strip.insertBefore(link, strip.firstChild);
+}
+
+getVideo();
+
+video.addEventListener('canplay', paintToCanvas);
