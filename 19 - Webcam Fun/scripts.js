9f88d6045ff73f415b40870d6c737bc665c9ae49
Ryan Rowland <w.rowland.ryan@gmail.com>
2019-05-20 14:14:52 -0500

    Day19 - add filters and green screening

--- 19 - Webcam Fun/scripts.js
+++ 19 - Webcam Fun/scripts.js
@@ -8,7 +8,6 @@ const snap = document.querySelector('.snap');
 async function getVideo() {
   try {
     const localMediaStream = await navigator.mediaDevices.getUserMedia({ video: true, audio: false });
-    console.log(localMediaStream);
     // video.src = window.URL.createObjectURL(localMediaStream);
     // ^^ shown in video, is deprecated now
     video.srcObject = localMediaStream;
@@ -29,7 +28,15 @@ function paintToCanvas() {
 
   return setInterval(() => {
     ctx.drawImage(video, 0, 0, width, height);
-    const pixels = ctx.getImageData(0, 0, width, height);
+    // take pixels out
+    let pixels = ctx.getImageData(0, 0, width, height);
+    // change pixels
+    // pixels = redEffect(pixels);
+    // pixels = rgbSplit(pixels);
+    // ctx.globalAlpha = 0.1; // ghosting effect
+    // put pixels back
+    greenScreen(pixels);
+    ctx.putImageData(pixels, 0, 0);
   }, 16);
 }
 
@@ -43,9 +50,56 @@ function takePhoto() {
   const link = document.createElement('a');
   link.href = data;
   link.setAttribute('download', 'handsome');
-  link.textContent = 'Download Image';
   link.innerHTML = `<img src="${data}" alt="Handsome photo" />`;
   strip.insertBefore(link, strip.firstChild);
+}
+
+function redEffect(pixels) {
+  for (let i = 0; i < pixels.data.length; i += 4) {
+    pixels.data[i] += 100; // red
+    pixels.data[i + 1] -= 50; // green
+    pixels.data[i + 2] *= 0.5; // blue
+    // pixels.data[i + 3]; // alpha
+  }
+  return pixels
+}
+
+function rgbSplit(pixels) {
+  for (let i = 0; i < pixels.data.length; i += 4) {
+    pixels.data[i - 150] = pixels.data[i]; // red
+    pixels.data[i + 100] = pixels.data[i + 1]; // green
+    pixels.data[i - 150] = pixels.data[i + 2]; // blue
+    // pixels.data[i + 3]; // alpha
+  }
+  return pixels
+}
+
+// copy and pasta
+function greenScreen(pixels) {
+  const levels = {};
+
+  document.querySelectorAll('.rgb input').forEach((input) => {
+    levels[input.name] = input.value;
+  });
+
+  for (i = 0; i < pixels.data.length; i = i + 4) {
+    red = pixels.data[i + 0];
+    green = pixels.data[i + 1];
+    blue = pixels.data[i + 2];
+    alpha = pixels.data[i + 3];
+
+    if (red >= levels.rmin
+      && green >= levels.gmin
+      && blue >= levels.bmin
+      && red <= levels.rmax
+      && green <= levels.gmax
+      && blue <= levels.bmax) {
+      // take it out!
+      pixels.data[i + 3] = 0;
+    }
+  }
+
+  return pixels;
 }
 
 getVideo();
